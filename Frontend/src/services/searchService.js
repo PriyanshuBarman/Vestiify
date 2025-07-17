7436809acedd459d0a1e9b3a4e46470f4bd7ac19
Priyanshu Barman <priyanshubarman741@gmail.com>
2025-07-17 11:40:12 +0530

    refactor(frontend): update search API URL to use environment variable

--- Frontend/src/services/searchService.js
+++ Frontend/src/services/searchService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const searchApi = import.meta.env.VITE_SEARCH_API_URL;
+
 export const fetchSearchResults = async (query) => {
-  const { data } = await axios.get(`https://api.kuvera.in/insight/api/v1/global_search.json?query=${query}`);
+  const { data } = await axios.get(`${searchApi}query=${query}`);
   return data.data.funds;
 };
